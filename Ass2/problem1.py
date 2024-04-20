454631f30a107fcd9cf34f3111ba20ec60b05314
ChackMociute <martynasvaznonis@gmail.com>
2024-04-20 20:55:28 +0200

    Finished problem 2

--- Ass2/problem1.py
+++ Ass2/problem1.py
@@ -8,12 +8,13 @@ NLP A2: N-Gram Language Models
 DO NOT SHARE/DISTRIBUTE SOLUTIONS WITHOUT THE INSTRUCTOR'S PERMISSION
 """
 
-word_index_dict = {}
-
 # TODO: read brown_vocab_100.txt into word_index_dict
+with open("brown_vocab_100.txt", 'r') as f:
+    word_index_dict = {w.rstrip(): i for i, w in enumerate(f.readlines())}
 
 # TODO: write word_index_dict to word_to_index_100.txt
-
+with open("word_to_index_100.txt", 'w') as f:
+    f.write(str(word_index_dict))
 
 
 print(word_index_dict['all'])
