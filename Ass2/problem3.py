d798ecceeada73265daa88fb091042c0711ce598
ChackMociute <martynasvaznonis@gmail.com>
2024-04-20 22:09:15 +0200

    Finished problem 3

--- Ass2/problem3.py
+++ Ass2/problem3.py
@@ -10,30 +10,28 @@ DO NOT SHARE/DISTRIBUTE SOLUTIONS WITHOUT THE INSTRUCTOR'S PERMISSION
 
 import numpy as np
 from sklearn.preprocessing import normalize
-from generate import GENERATE
-import random
-
-
-vocab = codecs.open("brown_vocab_100.txt")
 
 #load the indices dictionary
-word_index_dict = {}
-for i, line in enumerate(vocab):
-    #TODO: import part 1 code to build dictionary
+with open("word_to_index_100.txt", 'r') as f:
+    w2i = eval(f.read())
 
-f = codecs.open("brown_100.txt")
+with open("brown_100.txt", 'r') as f:
+    words = [w.lower() for s in f.readlines() for w in s.split()]
 
+counts = np.zeros((len(w2i), len(w2i)))
 
-counts = #TODO: initialize numpy 0s array
+for w1, w2 in zip(words[:-1], words[1:]):
+    counts[w2i[w1], w2i[w2]] += 1
 
-
-#TODO: iterate through file and update counts
-
-#TODO: normalize counts
-
-
-#TODO: writeout bigram probabilities
+probs = normalize(counts, norm='l1', axis=1)
 
 
+assert probs[w2i['all'], w2i['the']] == 1
+assert np.isclose(probs[w2i['the'], w2i['jury']], 0.08333333333333)
 
-f.close()
+np.savetxt("bigram_probs.txt", np.asarray([
+    probs[w2i['all'], w2i['the']],
+    probs[w2i['the'], w2i['jury']],
+    probs[w2i['the'], w2i['campaign']],
+    probs[w2i['anonymous'], w2i['calls']]
+]))
